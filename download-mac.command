e0285b5b0b4d0698002ba8022ace8754e8660053
Mamoru Sugihara <sugiharam2@gmail.com>
2024-10-28 13:03:25 +0900

    feat: 1.2.183 + 12.3.0

--- download-mac.command
+++ download-mac.command
@@ -6,14 +6,14 @@ pwd
 # https://developers.google.com/unity/archive
 
 array=()
-array+=(https://dl.google.com/games/registry/unity/com.google.external-dependency-manager/com.google.external-dependency-manager-1.2.182.tgz)
-array+=(https://dl.google.com/games/registry/unity/com.google.firebase.analytics/com.google.firebase.analytics-12.2.0.tgz)
-array+=(https://dl.google.com/games/registry/unity/com.google.firebase.app/com.google.firebase.app-12.2.0.tgz)
-array+=(https://dl.google.com/games/registry/unity/com.google.firebase.auth/com.google.firebase.auth-12.2.0.tgz)
-array+=(https://dl.google.com/games/registry/unity/com.google.firebase.crashlytics/com.google.firebase.crashlytics-12.2.0.tgz)
-array+=(https://dl.google.com/games/registry/unity/com.google.firebase.firestore/com.google.firebase.firestore-12.2.0.tgz)
-array+=(https://dl.google.com/games/registry/unity/com.google.firebase.functions/com.google.firebase.functions-12.2.0.tgz)
-array+=(https://dl.google.com/games/registry/unity/com.google.firebase.messaging/com.google.firebase.messaging-12.2.0.tgz)
+array+=(https://dl.google.com/games/registry/unity/com.google.external-dependency-manager/com.google.external-dependency-manager-1.2.183.tgz)
+array+=(https://dl.google.com/games/registry/unity/com.google.firebase.analytics/com.google.firebase.analytics-12.3.0.tgz)
+array+=(https://dl.google.com/games/registry/unity/com.google.firebase.app/com.google.firebase.app-12.3.0.tgz)
+array+=(https://dl.google.com/games/registry/unity/com.google.firebase.auth/com.google.firebase.auth-12.3.0.tgz)
+array+=(https://dl.google.com/games/registry/unity/com.google.firebase.crashlytics/com.google.firebase.crashlytics-12.3.0.tgz)
+array+=(https://dl.google.com/games/registry/unity/com.google.firebase.firestore/com.google.firebase.firestore-12.3.0.tgz)
+array+=(https://dl.google.com/games/registry/unity/com.google.firebase.functions/com.google.firebase.functions-12.3.0.tgz)
+array+=(https://dl.google.com/games/registry/unity/com.google.firebase.messaging/com.google.firebase.messaging-12.3.0.tgz)
 
 for var in ${array[@]}; do
   filename="$(basename ${var})"
--- download-win.bat
+++ download-win.bat
@@ -1,31 +1,31 @@
-if not exist %CD%\com.google.external-dependency-manager-1.2.182.tgz (
-bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.external-dependency-manager/com.google.external-dependency-manager-1.2.182.tgz %CD%\com.google.external-dependency-manager-1.2.182.tgz
+if not exist %CD%\com.google.external-dependency-manager-1.2.183.tgz (
+bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.external-dependency-manager/com.google.external-dependency-manager-1.2.183.tgz %CD%\com.google.external-dependency-manager-1.2.183.tgz
 )
 
-if not exist %CD%\com.google.firebase.analytics-12.2.0.tgz (
-bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.analytics/com.google.firebase.analytics-12.2.0.tgz %CD%\com.google.firebase.analytics-12.2.0.tgz
+if not exist %CD%\com.google.firebase.analytics-12.3.0.tgz (
+bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.analytics/com.google.firebase.analytics-12.3.0.tgz %CD%\com.google.firebase.analytics-12.3.0.tgz
 )
 
-if not exist %CD%\com.google.firebase.app-12.2.0.tgz (
-bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.app/com.google.firebase.app-12.2.0.tgz %CD%\com.google.firebase.app-12.2.0.tgz
+if not exist %CD%\com.google.firebase.app-12.3.0.tgz (
+bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.app/com.google.firebase.app-12.3.0.tgz %CD%\com.google.firebase.app-12.3.0.tgz
 )
 
-if not exist %CD%\com.google.firebase.auth-12.2.0.tgz (
-bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.auth/com.google.firebase.auth-12.2.0.tgz %CD%\com.google.firebase.auth-12.2.0.tgz
+if not exist %CD%\com.google.firebase.auth-12.3.0.tgz (
+bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.auth/com.google.firebase.auth-12.3.0.tgz %CD%\com.google.firebase.auth-12.3.0.tgz
 )
 
-if not exist %CD%\com.google.firebase.crashlytics-12.2.0.tgz (
-bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.crashlytics/com.google.firebase.crashlytics-12.2.0.tgz %CD%\com.google.firebase.crashlytics-12.2.0.tgz
+if not exist %CD%\com.google.firebase.crashlytics-12.3.0.tgz (
+bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.crashlytics/com.google.firebase.crashlytics-12.3.0.tgz %CD%\com.google.firebase.crashlytics-12.3.0.tgz
 )
 
-if not exist %CD%\com.google.firebase.firestore-12.2.0.tgz (
-bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.firestore/com.google.firebase.firestore-12.2.0.tgz %CD%\com.google.firebase.firestore-12.2.0.tgz
+if not exist %CD%\com.google.firebase.firestore-12.3.0.tgz (
+bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.firestore/com.google.firebase.firestore-12.3.0.tgz %CD%\com.google.firebase.firestore-12.3.0.tgz
 )
 
-if not exist %CD%\com.google.firebase.functions-12.2.0.tgz (
-bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.functions/com.google.firebase.functions-12.2.0.tgz %CD%\com.google.firebase.functions-12.2.0.tgz
+if not exist %CD%\com.google.firebase.functions-12.3.0.tgz (
+bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.functions/com.google.firebase.functions-12.3.0.tgz %CD%\com.google.firebase.functions-12.3.0.tgz
 )
 
-if not exist %CD%\com.google.firebase.messaging-12.2.0.tgz (
-bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.messaging/com.google.firebase.messaging-12.2.0.tgz %CD%\com.google.firebase.messaging-12.2.0.tgz
+if not exist %CD%\com.google.firebase.messaging-12.3.0.tgz (
+bitsadmin /transfer dl1 https://dl.google.com/games/registry/unity/com.google.firebase.messaging/com.google.firebase.messaging-12.3.0.tgz %CD%\com.google.firebase.messaging-12.3.0.tgz
 )
